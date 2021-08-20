8367cb6a819ae17042cd893fb94bdfe3d9f14a49
Ryan Volkert <rvolk75@gmail.com>
2021-08-20 16:54:36 -0600

    Added forecasting

--- dashboard.py
+++ dashboard.py
@@ -4,11 +4,12 @@ import dash_html_components as html
 import pandas as pd
 from datetime import datetime
 import plotly.graph_objects as go
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 from sklearn.linear_model import LinearRegression
 from Cross import Cross
+from Forecaster import Forecaster
 
-data = pd.read_excel('~/OneDrive/Consumption file/Consume + Ship Data - RV edits.xlsx', sheet_name='Main')
+data = pd.read_excel('Analysis Data.xlsx', sheet_name='Main')
 data = data.loc[data.PartNumber.notnull()]
 data.dropna(axis=1, how='all', inplace=True)
 data.set_index('PartNumber', inplace=True)
@@ -25,6 +26,9 @@ use_only.set_index('Date', inplace=True)
 use_only = use_only[use_only.columns[(use_only != 0).any()]]
 part_nums = use_only.columns
 
+categories = pd.read_excel('Analysis Data.xlsx', sheet_name='Categories', index_col=0)
+cat_opts = categories['Sales category'].dropna().unique()
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
@@ -40,6 +44,23 @@ app.layout = html.Div([
 
     html.Br(),
 
+    dcc.Checklist(
+        id='options',
+        options=[{'label': i, 'value': i} for i in cat_opts],
+        value=[],
+        labelStyle={'display': 'inline-block'},
+        inputStyle={'margin-left': '20px'}
+    ),
+
+    html.Br(),
+
+    html.Button('Forecast', id='btn', n_clicks=0),
+    html.Div(id='container', children='Select categories and press forecast'),
+
+    html.Br(),
+
+    html.Button('Reload', id='reload', n_clicks=0),
+
     dcc.Graph(id='part_numbers'),
     dcc.Graph(id='cross')
 ])
@@ -48,8 +69,12 @@ app.layout = html.Div([
 @app.callback(
     [Output('part_numbers', 'figure'),
      Output('cross', 'figure')],
-    Input('dropdown', 'value'))
-def update_graph(part_num):
+    [Input('dropdown', 'value'),
+     Input('reload', 'n_clicks')])
+def update_graph(part_num, n_clicks):
+    forecasts = pd.read_excel('Analysis Data.xlsx', sheet_name='Forecasts', index_col=0)
+    predictions = pd.read_excel('Analysis Data.xlsx', sheet_name='Predictions', index_col=0)
+
     dff = use_only[part_num].reset_index()
 
     x = dff.index.values.reshape(-1, 1)
@@ -61,6 +86,12 @@ def update_graph(part_num):
     fig1 = go.Figure()
     fig1.add_trace(go.Scatter(x=dff.Date, y=preds1, name='Trend'))
     fig1.add_trace(go.Scatter(x=dff.Date, y=dff[part_num], name='Data'))
+
+    if part_num in forecasts.columns:
+        forecast = forecasts[part_num]
+        prediction = predictions[part_num].dropna()
+        forecast = prediction.append(forecast).sort_index()
+        fig1.add_trace(go.Scatter(x=forecast.index, y=forecast.values, name='Forecast'))
 
     cross_name = data.loc[part_num, 'Cross']
     if cross_name != 0:
@@ -77,11 +108,44 @@ def update_graph(part_num):
         fig2.add_trace(go.Scatter(x=crosses.index, y=preds2, name='Trend'))
         fig2.add_trace(go.Scatter(x=crosses.index, y=crosses.sum(axis=1), name='Data'))
     else:
-        fig2 = go.Scatter(x=dff.index, y=[0]*len(dff.index))
+        fig2 = go.Figure()
+        fig2.add_trace(go.Scatter(x=dff.index, y=[0]*len(dff.index)))
+
+    fig1.update_layout(
+        title='Part Usage Over Time',
+        yaxis_title=part_num
+    )
+
+    fig2.update_layout(
+        title='Cross Usage Over Time',
+        yaxis_title=cross_name
+    )
 
     return fig1, fig2
 
 
+@app.callback(
+    Output('container', 'children'),
+    Input('btn', 'n_clicks'),
+    State('options', 'value')
+)
+def forecast(n_clicks, options):
+    if not n_clicks:
+        return 'Press forecast when ready'
+
+    if not options:
+        return 'You must select a category'
+
+    pns = categories[categories['Sales category'].isin(options)].index
+    pns = pns[pns.isin(use_only.columns)]
+    dat = use_only[pns]
+
+    forecaster = Forecaster(dat, 5)
+    forecaster.forecast()
+    forecaster.to_excel()
+
+    return 'Forecasting complete. Click reload to see changes.'
+
+
 if __name__ == '__main__':
     app.run_server(debug=True)
-
